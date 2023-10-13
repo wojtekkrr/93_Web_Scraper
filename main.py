704d08619dc1d0724e51c0b016dd9a9c592bdb65
Wojtek2810 <75879048+Wojtek2810@users.noreply.github.com>
2023-10-13 13:08:13 +0200

    Gotowy program

--- main.py
+++ main.py
@@ -4,52 +4,47 @@ from selenium.webdriver.common.by import By
 from time import sleep
 import csv
 
+# Adres sterownika
 chrome_driver_path = "C:\development\chromedriver.exe"
 
+# Nie wyłączanie okna po zakończeniu skryptu
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
+# Wczytanie sterownika
 service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
+# Adres strony z której będą zczytywane dane
 driver.get("https://sofifa.com/players")
-
-# #Przełączenie na Iframe
-# driver.switch_to.frame("googlefcLoaded")
-
-#Zlokalizowanie przycisku akceptuj cookie
-# cokie_accept_button = driver.find_element(By.CSS_SELECTOR, '.fc-cta-consent')
-# cokie_accept_button.click()
 
 sleep(2)
 
-
-#Zlokalizowanie kolumny overall rating
+# Zlokalizowanie kolumny overall rating i jej kilknięcie
 overall_rating = driver.find_element(By.CSS_SELECTOR, 'th.col-oa[aria-label="Overall rating"]')
 overall_rating.click()
 
-#Utworzenie listy nazwisk
+# Utworzenie listy nazwisk
 players_surname = driver.find_elements(By.CSS_SELECTOR, 'td.col-name > a[role="tooltip"] > div.ellipsis')
 
 players_surname_text = []
 for surname in players_surname[:15]:
     players_surname_text.append(surname.text)
 
-print(players_surname_text)
-
-#Utworzenie listy statystyk
+# Utworzenie listy rating
 players_rating = driver.find_elements(By.CSS_SELECTOR, 'td.col-oa[data-col="oa"]')
 
 players_rating_text = []
 for rating in players_rating[:15]:
     players_rating_text.append(rating.text)
 
-# Zapisz nazwiska zawodników do pliku CSV
-with open('nazwiska_zawodnikow.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    # Zapisz nagłówek, jeśli go potrzebujesz
-    # csvwriter.writerow(['Nazwisko'])
-    # Zapisz nazwiska zawodników
-    for nazwisko in players_surname_text:
-        csvwriter.writerow([nazwisko])
+# Połącz nazwiska z oceną
+players_text = []
+for i in range(len(players_surname_text)):
+    players_text.append(players_surname_text[i] + " - " + players_rating_text[i])
 
+# Zapisz nazwiska zawodników do pliku CSV
+with open('top_players.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for text in players_text:
+        csvwriter.writerow([text])
